6dfb14823564a626ef516c2d692d134616681cab
davidfitzek <fitzek@protonmail.com>
2022-02-01 11:30:07 +0100

    add virtual env script

---
+++ setup_venv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+python3 -m venv .env
+source .env/bin/activate
+pip install --upgrade pip
+
+pip install ipykernel
+pip install matplotlib
+pip install networkx
+
+pip install pennylane 
+pip install pennylane-qiskit
+pip install autograd jax jaxlib
