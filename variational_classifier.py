2a54b9bcb87dd7262fce198d2b98e987e4f959c5
osclil <osclil@student.chalmers.se>
2022-02-09 14:21:39 +0100

    Added variational_classifier.py

---
+++ variational_classifier.py
@@ -0,0 +1,16 @@
+# variational_classifier.py
+
+import pennylane as qml
+from pennylane import numpy as np
+from pennylane.optimize import NesterovMomentumOptimizer
+
+# Labels, predictions are assumed to be of equal length
+def square_loss(labels, preds):
+    loss = sum((l - p) ** 2 for l, p in zip(labels, preds))
+    return loss / len(labels)
+
+# Labels, predictions are assumed to be of equal length
+def accuracy(labels, preds):
+    tol = 1e-5
+    loss = sum((l - p) < tol for l, p in zip(labels, preds))
+    return loss / len(labels)
