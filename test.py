125abf19da0627f6d59971312d0717bf2155f04c
Emejl07 <emilzaya@gmail.com>
2022-02-09 14:38:00 +0100

    Changed test

--- test.py
+++ test.py
@@ -2,5 +2,19 @@ import pennylane as qml
 from pennylane import numpy as np
 from pennylane.optimize import NesterovMomentumOptimizer
 
+dev = qml.device("default.qubit", wires=4)
 
-print("Hello world")
+def layer(W):
+
+    qml.Rot(W[0, 0], W[0, 1], W[0, 2], wires=0)
+    qml.Rot(W[1, 0], W[1, 1], W[1, 2], wires=1)
+    qml.Rot(W[2, 0], W[2, 1], W[2, 2], wires=2)
+    qml.Rot(W[3, 0], W[3, 1], W[3, 2], wires=3)
+
+    qml.CNOT(wires=[0, 1])
+    qml.CNOT(wires=[1, 2])
+    qml.CNOT(wires=[2, 3])
+    qml.CNOT(wires=[3, 0])
+
+
+print("Helwo")
