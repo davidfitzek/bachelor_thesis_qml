88ba9b9bcb0958e7154c484cd7805c4cc87525a7
Emil Zaya <emilzaya@Emils-MacBook-Pro.local>
2022-02-09 14:15:47 +0100

    Added test.py

--- setup_venv.sh
+++ setup_venv.sh
@@ -4,6 +4,8 @@ python3 -m venv .env
 source .env/bin/activate
 pip install --upgrade pip
 
+pip install -U scikit-learn
+
 pip install ipykernel
 pip install matplotlib
 pip install networkx
---
+++ test.py
@@ -0,0 +1,5 @@
+import pennylane as qml
+from pennylane import numpy as np
+from pennylane.optimize import NesterovMomentumOptimizer
+
+
