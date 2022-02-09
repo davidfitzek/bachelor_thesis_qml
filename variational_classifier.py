b3a9dab62c91886002162097e0fea6b2f819674c
osclil <osclil@student.chalmers.se>
2022-02-09 14:48:31 +0100

    Added layer function

--- variational_classifier.py
+++ variational_classifier.py
@@ -4,6 +4,22 @@ import pennylane as qml
 from pennylane import numpy as np
 from pennylane.optimize import NesterovMomentumOptimizer
 
+n_wires = 4
+
+dev = qml.device("default.qubit", wires = n_wires)
+
+# The layer for the circuit
+def layer(W):
+    n = len(W)
+
+    # Adds rotation matrices
+    for i, row in enumerate(W):
+        qml.Rot(row[0], row[1], row[2], wires = i)
+
+    # Adds controlled NOT matrices
+    for i in range(n):
+        qml.CNOT(wires = [i, i % n])
+
 # Labels, predictions are assumed to be of equal length
 def square_loss(labels, preds):
     loss = sum((l - p) ** 2 for l, p in zip(labels, preds))
