ee18ae43c62c79186b7a22d145db6bc9cf662a37
osclil <osclil@student.chalmers.se>
2022-02-24 16:59:48 +0100

    Fixed error in split_data

--- common.py
+++ common.py
@@ -17,12 +17,17 @@ def accuracy(labels, preds):
 # percentage is a number between 0 and 1 indicating what percentage should be test data
 def split_data(X, Y, percentage):
     n = len(Y)
-    indexes = np.random.choice(range(n), np.int64(percentage * n))
+    
+    indexes = np.arange(n)
+    
+    indexes_train = np.random.choice(indexes, np.int64(percentage * n), replace = False)
+    indexes_train = np.sort(indexes_train)
+    indexes_val = np.setdiff1d(indexes, indexes_train, assume_unique = True)
 
-    X_train = X[indexes]
-    X_val = X[~indexes]
-    Y_train = Y[indexes]
-    Y_val = Y[~indexes]
+    X_train = X[indexes_train]
+    X_val = X[indexes_val]
+    Y_train = Y[indexes_train]
+    Y_val = Y[indexes_val]
 
     return X_train, X_val, Y_train, Y_val
 
