cace906ae231233e0a37e9a4a3de016e8093d175
osclil <osclil@student.chalmers.se>
2022-02-24 15:16:07 +0100

    Added imports

---
+++ sk_iris_classifier.py
@@ -0,0 +1,9 @@
+# sk_iris_classifier.py
+
+import pennylane as qml
+from pennylane import numpy as np
+from pennylane.optimize import NesterovMomentumOptimizer
+from sklearn.datasets import load_iris
+
+import common as com
+
