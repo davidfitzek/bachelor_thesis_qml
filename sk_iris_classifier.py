f1553e74e0700457d538e10a25d7046c546d6f95
osclil <osclil@student.chalmers.se>
2022-02-24 15:41:09 +0100

    Added code from iris classifier

--- sk_iris_classifier.py
+++ sk_iris_classifier.py
@@ -15,6 +15,55 @@ n_layers = 4
 
 dev = qml.device("default.qubit", wires = 2)
 
+# The layer for the circuit
+def layer(W):
+    n = len(W)
+
+    # Adds rotation matrices
+    for i, row in enumerate(W):
+        qml.Rot(row[0], row[1], row[2], wires = i)
+
+    # Adds controlled NOT matrices
+    for i in range(n):
+        qml.CNOT(wires = [i, (i + 1) % n])
+
+# TODO: Understand what these five angles are
+def get_angles(x):
+
+    beta_0 = 2 * np.arcsin(np.sqrt(x[1] ** 2) / np.sqrt(x[0] ** 2 + x[1] ** 2 + 1e-12))
+    beta_1 = 2 * np.arcsin(np.sqrt(x[3] ** 2) / np.sqrt(x[2] ** 2 + x[3] ** 2 + 1e-12))
+    beta_2 = 2 * np.arcsin(np.sqrt(x[2] ** 2 + x[3] ** 2) / np.sqrt(x[0] ** 2 + x[1] ** 2 + x[2] ** 2 + x[3] ** 2))
+
+    return np.array([beta_2, - beta_1 / 2, beta_1 / 2, - beta_0 / 2, beta_0 / 2])
+
+def statepreparation(angles):
+
+    qml.RY(angles[0], wires = 0)
+
+    for i in range(2):
+        for j in range(2):
+            qml.CNOT(wires = [0, 1])
+            qml.RY(angles[2 * i + j + 1], wires = 1)
+        qml.PauliX(wires = 0)
+
+# The circuit
+@qml.qnode(dev)
+def circuit(weights, angles):
+
+    statepreparation(angles)
+
+    for W in weights:
+        layer(W)
+
+    return qml.expval(qml.PauliZ(0))
+
+def variational_classifier(weights, angles, bias):
+    return circuit(weights, angles) + bias
+
+def cost(weights, bias, features, labels):
+    preds = [variational_classifier(weights, feature, bias) for feature in features]
+    return com.square_loss(labels, preds)
+
 data = load_iris()
 
 features = data['data']
