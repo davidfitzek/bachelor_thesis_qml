eb159c557aca4a8c7113eaacadaa190a03ea6c1f
osclil <osclil@student.chalmers.se>
2022-02-25 14:35:00 +0100

    Read compressed data

--- sk_iris_plot.py
+++ sk_iris_plot.py
@@ -4,4 +4,13 @@ from pennylane import numpy as np
 
 import matplotlib.pyplot as plt
 
+import json
+import lzma
+
+filename = 'data/sk_iris_result.json.xz'
+doc = {}
+
+with lzma.open(filename, 'r') as f:
+    filedata = f.read()
+    doc = json.loads(filedata)
 
