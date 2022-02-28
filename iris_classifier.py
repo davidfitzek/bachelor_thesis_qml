35d0592237d2e792153ca72261a182d0fd75b747
osclil <osclil@student.chalmers.se>
2022-02-28 18:47:23 +0100

    Fixed typo and started working on more general get_angles function. WIP, not working

--- iris_classifier.py
+++ iris_classifier.py
@@ -26,7 +26,6 @@ def layer(W):
     for i in range(n):
         qml.CNOT(wires = [i, (i + 1) % n])
 
-# TODO: Understand what three angles mean
 def get_angles(x):
     beta_0 = 2 * np.arcsin(np.linalg.norm(x[1]) / np.linalg.norm(x[: 2]))
     beta_1 = 2 * np.arcsin(np.linalg.norm(x[3]) / np.linalg.norm(x[2 :]))
@@ -34,12 +33,41 @@ def get_angles(x):
     
     return np.array([beta_2, - beta_1 / 2, beta_1 / 2, - beta_0 / 2, beta_0 / 2])
 
+# Looking at equation 8 in https://arxiv.org/pdf/quant-ph/0407010.pdf
+# With n = number of qubits, and k = 1, 2, ..., n
+def get_angles3(x):
+
+    # Number of qubits needed to encode x
+    # Should be equal to n_qubits
+    n = np.int64(np.ceil(np.log2(len(x))))
+
+    # Matrix for holding our angles
+    beta = np.zeros(shape = (2 ** (n - 1), n))
+
+    for k in range(n):
+        for j in range(2 ** (n - k) - 1):
+            # Compute the numerator inside the arcsin
+            num = np.sqrt(sum(
+                np.abs(x[(2 * j + 1) * 2 ** k + l]) ** 2
+                    for l in range(2 ** k)
+            ))
+            # Compute the denomenator inside the arcsin
+            den = np.sqrt(sum(
+                np.abs(x[j * 2 ** (k + 1) + l]) ** 2
+                    for l in range(2 ** (k + 1))
+            ))
+            beta[j, k] = 2 * np.arcsin(num / den)
+            print(beta[j, k])
+            print(k, j)
+
+    return beta
+
 def statepreparation(angles):
 
     qml.RY(angles[0], wires = 0)
 
     # Should be the same as n_qubits
-    n = len(angels) // 2
+    n = len(angles) // 2
 
     for i in range(n):
         for j in range(n):
