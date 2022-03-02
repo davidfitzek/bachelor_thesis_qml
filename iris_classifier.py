ab36f11c8bb4bf3e10551c05389b66048f562b0e
osclil <osclil@student.chalmers.se>
2022-03-02 14:09:19 +0100

    Did some refactoring

--- iris_classifier.py
+++ iris_classifier.py
@@ -52,7 +52,14 @@ def get_angles(x):
             ))
             beta[j, k] = 2 * np.arcsin(num / den)
 
-    return beta
+    #return beta
+    return np.array([
+        beta[0, 1],
+        -beta[1, 1] / 2,
+        beta[1, 1] / 2,
+        -beta[0, 0] / 2,
+        beta[0, 0] / 2
+    ])
 
 def statepreparation(angles):
 
@@ -86,66 +93,66 @@ def cost(weights, bias, features, labels):
     preds = [variational_classifier(weights, feature, bias) for feature in features]
     return com.square_loss(labels, preds)
 
+def optimise(n_iter, X_train, X_val, Y_train, Y_val):
+
+    n_train = len(Y_train)
+
+    weights_init = 0.01 * np.random.randn(n_layers , n_qubits, 3, requires_grad = True)
+    bias_init = np.array(0.0, requires_grad = True)
+
+    opt = NesterovMomentumOptimizer(0.01)
+    batch_size = 5
+
+    # train the variational classifier
+    weights = weights_init
+    bias = bias_init
+
+    for i in range(n_iter):
+    
+        # Update the weights by one optimiser step
+        batch_index = np.random.randint(0, high = n_train, size = (batch_size, ))
+        X_train_batch = X_train[batch_index]
+        Y_train_batch = Y_train[batch_index]
+        weights, bias, _, _ = opt.step(cost, weights, bias, X_train_batch, Y_train_batch)
+
+        # Compute predictions on train and test set
+        predictions_train = [np.sign(variational_classifier(weights, x, bias)) for x in X_train]
+        predictions_val = [np.sign(variational_classifier(weights, x, bias)) for x in X_val]
+
+        # Compute accuracy on train and test set
+        accuracy_train = com.accuracy(Y_train, predictions_train)
+        accuracy_val = com.accuracy(Y_val, predictions_val)
+
+        print(
+            'Iteration: {:5d} | Cost: {:0.7f} | Accuracy train: {:0.7f} | Accuracy validation: {:0.7f} '
+            ''.format(i + 1, cost(weights, bias, features, Y), accuracy_train, accuracy_val)
+        )
+
 # Load the data
 data = np.loadtxt('data/iris_classes1and2_scaled.txt')
 X = data[:, 0 : 2]
-print('First X sample (original)'.ljust(28) + ': {}'.format(X[0]))
+#print('First X sample (original)'.ljust(28) + ': {}'.format(X[0]))
 
 # Pad the vectors to size 2^2 with constant values
 padding = 0.3 * np.ones((len(X), 1))
 X_pad = np.c_[np.c_[X, padding], np.zeros((len(X), 1))]
-print('First X sample (padded)'.ljust(28) + ': {}'.format(X_pad[0]))
+#print('First X sample (padded)'.ljust(28) + ': {}'.format(X_pad[0]))
 
 # Normalise each input
-norm = np.sqrt(np.sum(X_pad ** 2, -1))
+norm = np.linalg.norm(X_pad)
 X_normalised = (X_pad.T / norm).T
-print('First X sample (normalised)'.ljust(28) + ': {}'.format(X_normalised[0]))
+#print('First X sample (normalised)'.ljust(28) + ': {}'.format(X_normalised[0]))
 
 # Angles for state preparation are new features
 features = np.array([get_angles(x) for x in X_normalised], requires_grad = False)
-print('First features sample'.ljust(28) + ': {}'.format(features[0]))
+#print('First features sample'.ljust(28) + ': {}'.format(features[0]))
 
 Y = data[:, -1]
 
 np.random.seed(123) # Set seed again for reproducibility
 
 # Split data into train data and test data
-features_train, features_test, Y_train, Y_test = com.split_data(features, Y, 0.7)
+features_train, features_val, Y_train, Y_val = com.split_data(features, Y, 0.7)
 
-n_train = len(Y_train)
-n_test = len(Y_test)
-
-print('Number of train data points : {}'.format(n_train))
-print('Number of test data points  : {}'.format(n_test))
-
-weights_init = 0.01 * np.random.randn(n_layers , n_qubits, 3, requires_grad = True)
-bias_init = np.array(0.0, requires_grad = True)
-
-opt = NesterovMomentumOptimizer(0.01)
-batch_size = 5
-
-# train the variational classifier
-weights = weights_init
-bias = bias_init
-n_steps = 60
-for i in range(n_steps):
-    
-    # Update the weights by one optimiser step
-    batch_index = np.random.randint(0, high = n_train, size = (batch_size, ))
-    features_train_batch = features_train[batch_index]
-    Y_train_batch = Y_train[batch_index]
-    weights, bias, _, _ = opt.step(cost, weights, bias, features_train_batch, Y_train_batch)
-
-    # Compute predictions on train and test set
-    predictions_train = [np.sign(variational_classifier(weights, feature, bias)) for feature in features_train]
-    predictions_test = [np.sign(variational_classifier(weights, feature, bias)) for feature in features_test]
-
-    # Compute accuracy on train and test set
-    accuracy_train = com.accuracy(Y_train, predictions_train)
-    accuracy_test = com.accuracy(Y_test, predictions_test)
-
-    print(
-        'Iteration: {:5d} | Cost: {:0.7f} | Accuracy train: {:0.7f} | Accuracy test: {:0.7f} '
-        ''.format(i + 1, cost(weights, bias, features, Y), accuracy_train, accuracy_test)
-    )
-
+# Optimise the weights
+optimise(60, features_train, features_val, Y_train, Y_val)
