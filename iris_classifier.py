291cfcafd8658ce6b78602e002ba0be1149bb3e0
osclil <osclil@student.chalmers.se>
2022-03-10 14:38:48 +0100

    Removed get_angles and stateprep_ex

--- iris_classifier.py
+++ iris_classifier.py
@@ -38,55 +38,6 @@ def layer_ex2(weights):
         qml.Rot(row[0], row[1], row[2], wires = i)
         qml.CNOT(wires = [i, (i + 1) % n])
 
-# Looking at equation 8 in https://arxiv.org/pdf/quant-ph/0407010.pdf
-# With n = number of qubits, and k = 1, 2, ..., n
-def get_angles(x):
-
-    # Number of qubits needed to encode x
-    # Should be equal to n_qubits
-    # Should be a power of two
-    n = np.int64(np.ceil(np.log2(len(x))))
-
-    # Matrix for holding our angles
-    beta = np.zeros(shape = (2 ** (n - 1), n))
-
-    for k in range(n):
-        for j in range(2 ** (n - k - 1)):
-            # Compute the numerator inside the arcsin
-            num = np.sqrt(sum(
-                np.abs(x[(2 * j + 1) * 2 ** k + l]) ** 2
-                    for l in range(2 ** k)
-            ))
-            # Compute the denomenator inside the arcsin
-            den = np.sqrt(sum(
-                np.abs(x[j * 2 ** (k + 1) + l]) ** 2
-                    for l in range(2 ** (k + 1))
-            ))
-            beta[j, k] = 2 * np.arcsin(num / den)
-
-    #return beta
-    return np.array([
-        beta[0, 1],
-        -beta[1, 1] / 2,
-        beta[1, 1] / 2,
-        -beta[0, 0] / 2,
-        beta[0, 0] / 2
-    ])
- 
-def stateprep_ex(angles):
-
-	qml.RY(angles[0], wires = 0)
-
-	# Should be the same as n_qubits
-	n = len(angles) // 2
-
-	for i in range(n):
-		for j in range(n):
-			qml.CNOT(wires = [0, 1])
-			qml.RY(angles[2 * i + j + 1], wires = 1)
-
-		qml.PauliX(wires = 0)
-
 def stateprep_amplitude(features):
     wires = np.int64(np.ceil(np.log2(len(features))))
     qml.AmplitudeEmbedding(features = features, wires = range(wires), normalize = True)
