760f68865f8ee65b368264011beeac65ad26e1ac
Emil Zaya <emilzaya@gmail.com>
2022-03-26 21:34:14 +0100

    Testing the forest dataset

--- QKE_testing.py
+++ QKE_testing.py
@@ -18,3 +18,11 @@ from qiskit.tools.visualization import circuit_drawer
 from urllib3 import encode_multipart_formdata
 from qiskit_machine_learning.kernels import QuantumKernel
 from QKE_functions import *
+
+forest = datasets.fetch_covtype()
+forest_sample_train, forest_sample_test, forest_label_train, forest_label_test = train_test_split(
+    forest.data, forest.target, test_size=0.3, random_state=14)
+
+[forest_sample_train, forest_sample_test] = reduceClassDimensions(4, forest_sample_train, forest_sample_test)
+
+print(forest_sample_train, forest_label_train)
