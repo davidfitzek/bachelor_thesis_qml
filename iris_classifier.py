8d4e41001ebf76fba218d3972f84485300639254
victorhenkow <98809430+victorhenkow@users.noreply.github.com>
2022-03-26 21:08:28 +0100

    kod som testar vad som händer när antalet lager ändras

--- iris_classifier.py
+++ iris_classifier.py
@@ -9,6 +9,10 @@ from sklearn.model_selection import train_test_split
 
 import common as com
 
+import matplotlib.pyplot as plt
+
+import time
+
 np.random.seed(123) # Set seed for reproducibility
 
 # Collect this in a class
@@ -60,7 +64,7 @@ def cost_fun(weights, bias, features, labels, variational_classifier_fun):
 	preds = [variational_classifier_fun(weights, feature, bias) for feature in features]
 	return com.square_loss(labels, preds)
 
-def optimise(n_iter, weights, bias, data, data_train, data_val, circuit):
+def optimise(accuracy_stop, cost_stop, weights, bias, data, data_train, data_val, circuit, n_layers):
 	#opt = NesterovMomentumOptimizer(stepsize = 0.01) # Performs much better than GradientDescentOptimizer
 	opt = AdamOptimizer(stepsize = 0.01) # To be tried, was mentioned
 	batch_size = 5 # This might be something which can be adjusted
@@ -76,8 +80,10 @@ def optimise(n_iter, weights, bias, data, data_train, data_val, circuit):
 	# Number of training points, used when choosing batch indexes
 	n_train = len(data_train.Y)
 
-	for i in range(n_iter):
-
+	accuracy_val = 0.0
+	cost_var = 100
+	i = 0
+	while (accuracy_val < accuracy_stop) or (cost_var > cost_stop):
 		# Update the weights by one optimiser step
 		batch_index = np.random.randint(0, high = n_train, size = (batch_size, ))
 		X_train_batch = data_train.X[batch_index]
@@ -91,10 +97,15 @@ def optimise(n_iter, weights, bias, data, data_train, data_val, circuit):
 		accuracy_train = com.accuracy(data_train.Y, predictions_train)
 		accuracy_val = com.accuracy(data_val.Y, predictions_val)
 
+		cost_var = float(cost(weights, bias, data.X, data.Y))
+
 		print(
-			'Iteration: {:5d} | Cost: {:0.7f} | Accuracy train: {:0.7f} | Accuracy validation: {:0.7f} '
-			''.format(i + 1, cost(weights, bias, data.X, data.Y), accuracy_train, accuracy_val)
-		)
+			'Iteration: {:5d} | Cost: {:0.7f} | Accuracy train: {:0.7f} | Accuracy validation: {:0.7f} | Layers: {:d} '
+			''.format(i + 1, cost_var, accuracy_train, accuracy_val, n_layers))
+
+		i += 1
+
+	return [i, cost_var]
 
 # Split a data object into training and validation data
 # p is the proportion of the data which should be used for training
@@ -104,7 +115,7 @@ def split_data(data, p):
 
 	return Data(X_train, Y_train), Data(X_val, Y_val)
 
-def run_variational_classifier(n_qubits, n_layers, data, stateprep_fun, layer_fun):
+def run_variational_classifier(n_qubits, n_layers, data, stateprep_fun, layer_fun, accuracy_stop, cost_stop):
 
 	# The device and qnode used by pennylane
 	device = qml.device("default.qubit", wires = n_qubits)
@@ -119,12 +130,10 @@ def run_variational_classifier(n_qubits, n_layers, data, stateprep_fun, layer_fu
 
 	data_train, data_val = split_data(data, p)
 
-	n_iter = 100 # Number of iterations, should be changed to a tolerance based process instead
-
 	weights = 0.01 * np.random.randn(n_layers , n_qubits, 3, requires_grad = True) # Initial value for the weights
 	bias = np.array(0.0, requires_grad = True) # Initial value for the bias
 
-	optimise(n_iter, weights, bias, data, data_train, data_val, circuit)
+	return optimise(accuracy_stop, cost_stop, weights, bias, data, data_train, data_val, circuit, n_layers)
 
 # Load the iris data set from sklearn into a data object
 def load_data_iris():
@@ -177,7 +186,12 @@ def load_data_cancer():
 def main():
 
 	n_qubits = 2
-	n_layers = 6
+	#it will test all the number of layers up to this number
+	range_layers = 15
+
+	# if the accuracy validation is higher and the cost is lower the iterations stop
+	accuracy_stop = 0.95
+	cost_stop = 0.7
 
 	# Can be any function that takes an input vector and encodes it
 	stateprep_fun = stateprep_amplitude
@@ -188,13 +202,34 @@ def main():
 	# Load the iris data
 	data = load_data_iris()
 
-	run_variational_classifier(
-		n_qubits,
-		n_layers,
-		data,
-		stateprep_fun,
-		layer_fun
-	)
+	#testing how many layers it takes to achieve accuracy_stop and cost_stop
+	iterations = [0]*range_layers
+	cost = [0]*range_layers
+	sec = [0]*range_layers
+	for i in range(range_layers):
+		n_layers = i + 1
+		print("Starting with layer " + str(n_layers) + " of " + str(range_layers))
+		tic = time.perf_counter()
+		[iterations[i], cost[i]] = run_variational_classifier(n_qubits, n_layers, data, stateprep_fun, layer_fun, accuracy_stop, cost_stop)
+		toc = time.perf_counter()
+		sec[i] = toc - tic
+
+	plt.subplot(3, 1, 1)
+	plt.plot(iterations)
+	plt.xlabel("Layer")
+	plt.ylabel("Iterations")
+
+	plt.subplot(3, 1, 2)
+	plt.plot(cost)
+	plt.xlabel("Layer")
+	plt.ylabel("Cost")
+
+	plt.subplot(3, 1, 3)
+	plt.plot(sec)
+	plt.xlabel("Layer")
+	plt.ylabel("Seconds to execute")
+
+	plt.show()
 
 if __name__ == '__main__':
 	main()
