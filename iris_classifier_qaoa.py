4148e3faf3a3df252286fdfd779d82f9dd0470c8
osclil <osclil@student.chalmers.se>
2022-03-30 18:51:26 +0200

    Made iris_classifier_qaoa.py rely on data.py

--- iris_classifier_qaoa.py
+++ iris_classifier_qaoa.py
@@ -4,21 +4,12 @@ import pennylane as qml
 from pennylane import numpy as np
 import pennylane.optimize as opt
 
-from sklearn.datasets import load_iris, load_breast_cancer, fetch_covtype
-from sklearn.model_selection import train_test_split
-
 import common as com
+import data as dat
 
 import json
 
 np.random.seed(123) # Set seed for reproducibility
-
-# Collect this in a class
-class Data:
-
-	def __init__(self, X, Y):
-		self.X = X
-		self.Y = Y
 
 def circuit_QAOA(features, weights):
 	wires = len(features)
@@ -88,14 +79,6 @@ def optimise(n_iter, weights, bias, data, data_train, data_val, circuit):
 	with open('data/test.json', 'w') as f:
 		json.dump(doc, f)
 
-# Split a data object into training and validation data
-# p is the proportion of the data which should be used for training
-def split_data(data, p):
-
-	X_train, X_val, Y_train, Y_val = train_test_split(data.X, data.Y, train_size = p)
-
-	return Data(X_train, Y_train), Data(X_val, Y_val)
-
 def run_variational_classifier(n_qubits, n_layers, data, circuit_fun):
 
 	# The device and qnode used by pennylane
@@ -109,7 +92,7 @@ def run_variational_classifier(n_qubits, n_layers, data, circuit_fun):
 	# The proportion of the data which should be use for training
 	p = 0.7
 
-	data_train, data_val = split_data(data, p)
+	data_train, data_val = dat.split_data(data, p)
 
 	n_iter = 200 # Number of iterations, should be changed to a tolerance based process instead
 
@@ -117,90 +100,6 @@ def run_variational_classifier(n_qubits, n_layers, data, circuit_fun):
 	bias = np.array(0.0, requires_grad = True) # Initial value for the bias
 
 	optimise(n_iter, weights, bias, data, data_train, data_val, circuit)
-
-# Load the iris data set from sklearn into a data object
-def load_data_iris():
-
-	# Load the data set
-	data = load_iris()
-
-	X = data['data']
-	Y = data['target']
-
-	# We will only look at two types, -1 and 1
-	# In Y, elements are of three types 0, 1, and 2.
-	# We simply cutoff the 2:s for now
-	# The array is sorted so we can easily find first occurence of a 2 with binary search
-	cutoff = np.searchsorted(Y, 2)
-
-	# Now simply remove the x:s and y:s corresponding to the 2:s
-	X = X[: cutoff]
-	Y = Y[: cutoff]
-
-	# Scale and translate Y from 0 and 1 to -1 and 1
-	Y = 2 * Y - 1
-	Y = np.array(Y) # PennyLane numpy differ from normal numpy. Converts np.ndarray to pennylane.np.tensor.tensor
-
-	# PennyLane numpy differ from normal numpy.
-	# Converts np.ndarray to pennylane.np.tensor.tensor
-	Y = np.array(Y)
-	X = np.array([np.array(x) for x in X], requires_grad = False)
-
-	return Data(X, Y)
-
-def load_data_cancer():
-
-	# Load the data set
-	data = load_breast_cancer()
-
-	X = data['data']
-	Y = data['target']
-
-	# Scale and translate Y from 0 and 1 to -1 and 1
-	Y = 2 * Y - 1
-
-	# PennyLane numpy differ from normal numpy.
-	# Converts np.ndarray to pennylane.np.tensor.tensor
-	Y = np.array(Y)
-	X = np.array([np.array(x) for x in X], requires_grad = False)
-
-	return Data(X, Y)
-
-def load_data_forest():
-
-	# Load the data set
-	data = fetch_covtype()
-
-	X_raw = data['data']
-	Y_raw = data['target']
-
-	Y = []
-	X = []
-
-	# It turns out this will yield a dataset of 495 141 points
-	# We limit the size to the first 500 for now
-	# Might be replaced with a random sample instead
-
-	# In the forest data elements can be of type 1 to 7
-	# We only log at type 1 and type 2
-	cnt = 0
-	for x, y in zip(X_raw, Y_raw):
-		if y < 3:
-			X.append(np.array(x))
-			Y.append(y)
-			# Not 
-			cnt = cnt + 1
-			if cnt == 500:
-				break
-
-	# Convert to numpy array
-	Y = np.array(Y)
-	X = np.array(X)
-
-	# Scale and translate Y from 1 and 2 to -1 and 1
-	Y = 2 * Y - 3
-
-	return Data(X, Y)
 
 def main():
 
@@ -212,7 +111,7 @@ def main():
 
 
 	# Load the iris data
-	data = load_data_iris()
+	data = dat.load_data_iris()
 
 	run_variational_classifier(
 		n_qubits,
