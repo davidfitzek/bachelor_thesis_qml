c67d794281a20dd7331858430c2e55083901e49c
Emil Zaya <emilzaya@gmail.com>
2022-04-01 20:22:40 +0200

    The code is now very modular and is being used in the QKE code.

--- classicalSVM.py
+++ classicalSVM.py
@@ -1,41 +1,27 @@
-from sklearn import datasets
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
+from data import *
 
 def run_SVM(kernel_function,poly_degree,data,cross_fold):
-    X=data.data
-    Y=data.target
+    [sample_train, sample_test, label_train, label_test] = data
     for k in kernel_function:
         clf = SVC(kernel=k,degree=poly_degree,gamma='scale')
-
         if cross_fold<=1:
             #Calculates accuracy without cross validation
-            x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
-            clf.fit(x_train,y_train)
-            y_pred = clf.predict(x_test)
+            clf.fit(sample_train, label_train)
+            clf.fit(sample_train, label_train)
+            score = clf.score(sample_test, label_test)
             if k == 'poly':
-                print("%s accuracy: %0.2f, Cross_fold ammount: %0.2f, Poly_degree: %0.2f" % (k, accuracy_score(y_test, y_pred), cross_fold, poly_degree))
+                print("%s accuracy: %0.3f, Cross_fold ammount: %0.3f, Poly_degree: %0.3f" % (k, score, cross_fold, poly_degree))
             else:
-                print("%s accuracy: %0.2f, Cross_fold ammount: %0.2f" % (k, accuracy_score(y_test, y_pred), cross_fold))
+                print("%s accuracy: %0.3f, Cross_fold ammount: %0.3f" % (k, score, cross_fold))
         else:
             #Calculates accuracy with cross validation, and presents mean and standard deviation
-            scores=cross_val_score(clf,X,Y, cv=cross_fold)
+            scores=cross_val_score(clf,sample_test,label_test, cv=cross_fold)
             if k == 'poly':
-                print("%s accuracy: %0.2f ± %0.2f, Cross_fold ammount: %0.2f, Poly_degree: %0.2f" % (k, scores.mean(), scores.std(), cross_fold, poly_degree))
+                print("%s accuracy: %0.3f ± %0.3f, Cross_fold ammount: %0.3f, Poly_degree: %0.3f" % (k, scores.mean(), scores.std(), cross_fold, poly_degree))
             else:
-                print("%s accuracy: %0.2f ± %0.2f, Cross_fold ammount: %0.2f" % (k, scores.mean(), scores.std(), cross_fold))
-
-def load_data_iris():
-    iris = datasets.load_iris()
-
-    return iris
-
-def load_data_cancer():
-    breast_cancer = datasets.load_breast_cancer()
-    return breast_cancer
+                print("%s accuracy: %0.3f ± %0.3f, Cross_fold ammount: %0.3f" % (k, scores.mean(), scores.std(), cross_fold))
 
 def main():
     #Kernel choices:‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’
@@ -43,7 +29,7 @@ def main():
     #Degree of poly
     poly_degree=2
 
-    data=load_data_iris()
+    data=load_data_iris(100)
 
     #Amount of parts the data is divided into for cross validation
     #The runtime will be increased by a factor of this number roughly
