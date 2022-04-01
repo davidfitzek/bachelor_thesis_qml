b85d0cf250b9a2fe99c2810feb12225169917340
osclil <osclil@student.chalmers.se>
2022-04-01 16:53:58 +0200

    Made mkfig.py work with new cross validated data

--- mkfig.py
+++ mkfig.py
@@ -1,26 +1,68 @@
 # mkfig.py
 
+import statistics as stat
+
 import sys
 import json
 import matplotlib.pyplot as plt
+
+def getdata(data, key):
+
+	# Figure out number of iterations based on data length
+	iters = [_ for _ in range(len(data['cross iter1'][key]))]
+
+	res = [[cross_val_data[key][iter] for _, cross_val_data in data.items()] for iter in iters]
+
+	return res
+
+def getstat(vals):
+	mean = [stat.mean(val) for val in vals]
+	stdev = [stat.stdev(val, xbar = m) for val, m in zip(vals, mean)]
+
+	return mean, stdev
 
 def mkfig(jsonfile):
 	data = {}
 	with open(jsonfile, 'r') as infile:
 		data = json.load(infile)
 
+	key_labels = {
+		'costs': 'cost',
+		'acc_train': 'accuracy training',
+		'acc_val': 'accuracy validation'
+	}
+
+	key_colours = {
+		'costs': 'blue',
+		'acc_train': 'green',
+		'acc_val': 'orange'
+	}
+
 	# Figure out number of iterations based on data length
-	iters = [_ for _ in range(len(data['costs']))]
+	iters = [_ for _ in range(len(data['cross iter1']['costs']))]
 
 	plt.clf()
 
-	plt.plot(iters, data['costs'], label = 'cost')
-	plt.plot(iters, data['acc_train'], label = 'accuracy training')
-	plt.plot(iters, data['acc_val'], label = 'accuracy validation')
+	for key, label in key_labels.items():
+		vals = getdata(data, key)
+		mean, stdev = getstat(vals)
+		upper = [m + e for m, e in zip(mean, stdev)]
+		lower = [m - e for m, e in zip(mean, stdev)]
+
+		# Plot mean value
+		plt.plot(iters, mean, label = label, color = key_colours[key], alpha = 1)
+
+		# Plot upper and lower bound
+		plt.plot(iters, lower, color = key_colours[key], alpha = 0.3)
+		plt.plot(iters, upper, color = key_colours[key], alpha = 0.3)
+
+		# Plot error region
+		plt.fill_between(iters, lower, upper, color = key_colours[key], alpha = 0.1)
 
 	plt.xlabel('iterations')
 
 	plt.legend()
+	#plt.show()
 	plt.savefig('figs' + jsonfile[4 : -4] + 'pdf')
 
 def main():
