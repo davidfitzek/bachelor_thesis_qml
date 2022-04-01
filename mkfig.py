7a7820624836c218e089fe1fa691f7e3aafb4b87
osclil <osclil@student.chalmers.se>
2022-04-01 17:40:07 +0200

    mkfig only looks at accuracy on validation data, and scaled both axises

--- mkfig.py
+++ mkfig.py
@@ -5,6 +5,7 @@ import statistics as stat
 import sys
 import json
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 def getdata(data, key):
 
@@ -21,6 +22,34 @@ def getstat(vals):
 
 	return mean, stdev
 
+def getbounds(vals):
+	mean = [stat.mean(val) for val in vals]
+	maxv = [max(val) for val in vals]
+	minv = [min(val) for val in vals]
+
+	return mean, maxv, minv
+
+def prepdata(data, key):
+	vals = getdata(data, key)
+	mean, stdev = getstat(vals)
+	upper = [m + e for m, e in zip(mean, stdev)]
+	lower = [m - e for m, e in zip(mean, stdev)]
+
+	return mean, upper, lower
+
+def plot(fig, iters, mean, upper, lower, label, colour):
+	# Plot mean value
+	fig.plot(iters, mean, label = label, color = colour, alpha = 1)
+
+	# Plot upper and lower bound
+	fig.plot(iters, lower, color = colour, alpha = 0.3)
+	fig.plot(iters, upper, color = colour, alpha = 0.3)
+
+	# Plot error region
+	fig.fill_between(iters, lower, upper, color = colour, alpha = 0.1)
+
+	fig.tick_params(axis = 'y')
+
 def mkfig(jsonfile):
 	data = {}
 	with open(jsonfile, 'r') as infile:
@@ -29,13 +58,13 @@ def mkfig(jsonfile):
 	key_labels = {
 		'costs': 'cost',
 		'acc_train': 'accuracy training',
-		'acc_val': 'accuracy validation'
+		'acc_val': 'accuracy'
 	}
 
 	key_colours = {
 		'costs': 'blue',
-		'acc_train': 'green',
-		'acc_val': 'orange'
+		'acc_train': 'orange',
+		'acc_val': 'green'
 	}
 
 	# Figure out number of iterations based on data length
@@ -43,25 +72,27 @@ def mkfig(jsonfile):
 
 	plt.clf()
 
-	for key, label in key_labels.items():
-		vals = getdata(data, key)
-		mean, stdev = getstat(vals)
-		upper = [m + e for m, e in zip(mean, stdev)]
-		lower = [m - e for m, e in zip(mean, stdev)]
+	fig, ax1 = plt.subplots()
 
-		# Plot mean value
-		plt.plot(iters, mean, label = label, color = key_colours[key], alpha = 1)
+	ax1.set_xlabel('iterations')
+	ax1.set_ylabel(key_labels['costs'])
 
-		# Plot upper and lower bound
-		plt.plot(iters, lower, color = key_colours[key], alpha = 0.3)
-		plt.plot(iters, upper, color = key_colours[key], alpha = 0.3)
+	cost_mean, cost_upper, cost_lower = prepdata(data, 'costs')
 
-		# Plot error region
-		plt.fill_between(iters, lower, upper, color = key_colours[key], alpha = 0.1)
+	plot(ax1, iters, cost_mean, cost_upper, cost_lower, key_labels['costs'], key_colours['costs'])
 
-	plt.xlabel('iterations')
+	ax2 = ax1.twinx()
+	ax2.set_ylabel(key_labels['acc_val'])
 
-	plt.legend()
+	acc_mean, acc_upper, acc_lower = prepdata(data, 'acc_val')
+
+	plot(ax2, iters, acc_mean, acc_upper, acc_lower, key_labels['acc_val'], key_colours['acc_val'])
+
+	l1 = mpatches.Patch(color = key_colours['costs'], label = key_labels['costs'])
+	l2 = mpatches.Patch(color = key_colours['acc_val'], label = key_labels['acc_val'])
+
+	plt.legend(handles = [l1, l2], loc = 'upper right')
+
 	#plt.show()
 	plt.savefig('figs' + jsonfile[4 : -4] + 'pdf')
 
