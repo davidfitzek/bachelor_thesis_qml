0f8c21e9298c6885d93ba88c1d1c61bf8ac83a64
Emil Zaya <emilzaya@gmail.com>
2022-04-02 19:59:07 +0200

    Added more comments

--- QKE_functions.py
+++ QKE_functions.py
@@ -3,32 +3,36 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, minmax_scale
 import numpy as np
 
+#This function reduces the amount of attributes of the dataset.
+#This is done with principal component analysis (PCA).
 def reduceAttributeDimensions(n_dim, sample_train, sample_test):
-    # Reduce dimensions
+    # Reduce dimensions with PCA
     pca = PCA(n_components=n_dim).fit(sample_train)
     sample_train = pca.transform(sample_train)
     sample_test = pca.transform(sample_test)
-    # Normalise
+    # Normalise the data
     std_scale = StandardScaler().fit(sample_train)
     sample_train = std_scale.transform(sample_train)
     sample_test = std_scale.transform(sample_test)
-    # Scale
+    # Scale the data to (-1, 1)
     digits_samples = np.append(sample_train, sample_test, axis=0)
     minmax_scale = MinMaxScaler((-1, 1)).fit(digits_samples)
     sample_train = minmax_scale.transform(sample_train)
     sample_test = minmax_scale.transform(sample_test)
+
     return sample_train, sample_test
 
-def reduceClassDimensions(X_raw, Y_raw, n_class, n_data):
+#This function reduces the amount of classes of the dataset.
+#This is done by only selecting the labels we want to use.
+def reduceClassDimensions(X_raw, Y_raw, n_classes, n_data):
     X = []
     Y = []
 
     cnt = 0
     for x, y in zip(X_raw, Y_raw):
-        if y <= n_class:
+        if y <= n_classes:
             X.append(np.array(x))
             Y.append(y)
-
             cnt = cnt + 1
             if cnt == n_data:
                 break
--- classicalSVM.py
+++ classicalSVM.py
@@ -12,21 +12,20 @@ def run_SVM(kernel_function,poly_degree,data,cross_fold):
             clf.fit(sample_train, label_train)
             score = clf.score(sample_test, label_test)
             if k == 'poly':
-                print("%s accuracy: %0.3f, Cross_fold ammount: %0.3f, Poly_degree: %0.3f" % (k, score, cross_fold, poly_degree))
+                print("%s accuracy: %0.3f, Cross_fold ammount: %0.1f, Poly_degree: %0.1f" % (k, score, cross_fold, poly_degree))
             else:
-                print("%s accuracy: %0.3f, Cross_fold ammount: %0.3f" % (k, score, cross_fold))
+                print("%s accuracy: %0.3f, Cross_fold ammount: %0.1f" % (k, score, cross_fold))
         else:
             #Calculates accuracy with cross validation, and presents mean and standard deviation
             scores=cross_val_score(clf,sample_test,label_test, cv=cross_fold)
             if k == 'poly':
-                print("%s accuracy: %0.3f ± %0.3f, Cross_fold ammount: %0.3f, Poly_degree: %0.3f" % (k, scores.mean(), scores.std(), cross_fold, poly_degree))
+                print("%s accuracy: %0.3f ± %0.3f, Cross_fold ammount: %0.1f, Poly_degree: %0.1f" % (k, scores.mean(), scores.std(), cross_fold, poly_degree))
             else:
-                print("%s accuracy: %0.3f ± %0.3f, Cross_fold ammount: %0.3f" % (k, scores.mean(), scores.std(), cross_fold))
+                print("%s accuracy: %0.3f ± %0.3f, Cross_fold ammount: %0.1f" % (k, scores.mean(), scores.std(), cross_fold))
 
 def main():
-    #Kernel choices:‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’
     kernel_function=['linear', 'poly', 'rbf', 'sigmoid']
-    #Degree of poly
+
     poly_degree=2
 
     data=load_data_iris(100)
